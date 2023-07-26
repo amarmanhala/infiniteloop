87cd214951cef93268d143093a51e0346c9a8a76
amarmanhala <amar.manhala@gmail.com>
2023-07-26 13:09:34 -0400

    Add playlist page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,46 @@
-import Link from "next/link"
-import { Inter, IBM_Plex_Serif } from 'next/font/google'
+import Link from "next/link";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 
-const ibmPlexSerif = IBM_Plex_Serif({ weight: ['400'], subsets: ['latin'], })
+const ibmPlexSerif = IBM_Plex_Serif({ weight: ["300"], subsets: ["latin"] });
 const Header = () => {
   return (
     <header className="flex flex-row justify-between w-full text-neutral-300">
-      <nav className={`text-xl ${ibmPlexSerif.className} w-full`}>
-      <ul className="">
-      <li className="ml-4  pb-1 mb-2 flex justify-between items-center font-semibold"><Link href="/" className="border-b-2 border-neutral-300">Amarpreet Singh</Link></li>
-          <li className="ml-4 pb-1 mb-2 flex justify-between items-center"><Link href="./projects" className="border-b-2 border-neutral-300">Projects</Link></li>
-          <li className="ml-4 pb-1 mb-2 flex justify-between items-center"><Link href="./about" className="border-b-2 border-neutral-300">About</Link></li>
-          <li className="ml-4 pb-1 mb-2 flex justify-between items-center"><Link href="./projects" className="border-b-2 border-neutral-300">Resume</Link></li>
-          <li className="ml-4 pb-1 mb-2 flex justify-between items-center"><Link href="./contact" className="border-b-2 border-neutral-300">Let&apos;s connect</Link></li>
+      <nav className={`text-xl ${ibmPlexSerif.className} w-full text-lg`}>
+        <ul className="">
+          <li className="ml-4  pb-1 mb-1 flex justify-between items-center">
+            <Link href="/" className="border-b border-neutral-300">
+              Amarpreet Singh
+            </Link>
+          </li>
+          <li className="ml-4 pb-1 mb-1 flex justify-between items-center font-normal">
+            <Link href="./projects" className="border-b border-neutral-300">
+              Projects
+            </Link>
+          </li>
+          <li className="ml-4 pb-1 mb-1 flex justify-between items-center">
+            <Link href="./about" className="border-b border-neutral-300">
+              About
+            </Link>
+          </li>
+          <li className="ml-4 pb-1 mb-1 flex justify-between items-center">
+            <Link href="./projects" className="border-b border-neutral-300">
+              Resume
+            </Link>
+          </li>
+          <li className="ml-4 pb-1 mb-1 flex justify-between items-center">
+            <Link href="./projects" className="border-b border-neutral-300">
+              Playlist
+            </Link>
+          </li>
+          <li className="ml-4 pb-1 mb-1 flex justify-between items-center">
+            <Link href="./contact" className="border-b border-neutral-300">
+              Let&apos;s connect
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
