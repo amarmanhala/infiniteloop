d38cdc5c9169380eb28f86a2daaf9409d2ac86a4
amarmanhala <amar.manhala@gmail.com>
2023-10-01 23:12:05 -0400

    Re-designed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} p-6`}>
-        <Header />
+        
         {children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,16 @@ const ibmPlexSerif = IBM_Plex_Serif({ weight: ["300"], subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col px-6 py-10 sm:px-32 sm:py-20">
-      <section>
-        <h1 className="text-2xl sm:text-4xl text-neutral-300 mb-6 font-medium">
-          Amarpreet Singh
+    <main className="flex min-h-screen flex-col px-6 py-8 sm:px-6 sm:py-6">
+      <section className="">
+        <h1 className="text-5xl text-white font-regular leading-tight">
+        Hello, I&apos;m Amarpreet Singh, a software developer and designer based in chilly Toronto. I spend my time to crafting delightful software solutions that help companies grow their businesses.
+          
         </h1>
-        <p className="text-2xl sm:text-2xl text-neutral-300 font-light">
-          Hello! I am software engineer, designer. I love building delightful
-          products.
-        </p>
+       
       </section>
 
-      <section className="mt-24 sm:mt-32">
+      <section className="mt-12">
         <h2
           className={`${ibmPlexSerif.className} text-neutral-300 text-2xl sm:text-4xl border-b border-neutral-300 py-3 sm:py-6`}
         >
